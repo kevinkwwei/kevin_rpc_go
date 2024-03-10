161e9bbdb97c7ba6563f1ec0ab291143a9ff0558
kevinkwwei <kevinweikk@gmail.com>
2024-03-10 23:15:13 +0800

    tansport options init

---
+++ rpc_transport/client_transport_options.go
@@ -0,0 +1,41 @@
+package rpc_transport
+
+import "time"
+
+type ClientTransportOptions struct {
+	Target      string
+	ServiceName string
+	Network     string
+
+	Timeout time.Duration
+}
+
+type ClientTransportOption func(*ClientTransportOptions)
+
+// WithServiceName returns a ClientTransportOption which sets the value for serviceName
+func WithServiceName(serviceName string) ClientTransportOption {
+	return func(o *ClientTransportOptions) {
+		o.ServiceName = serviceName
+	}
+}
+
+// WithClientTarget returns a ClientTransportOption which sets the value for target
+func WithClientTarget(target string) ClientTransportOption {
+	return func(o *ClientTransportOptions) {
+		o.Target = target
+	}
+}
+
+// WithClientNetwork returns a ClientTransportOption which sets the value for network
+func WithClientNetwork(network string) ClientTransportOption {
+	return func(o *ClientTransportOptions) {
+		o.Network = network
+	}
+}
+
+// WithTimeout returns a ClientTransportOption which sets the value for timeout
+func WithTimeout(timeout time.Duration) ClientTransportOption {
+	return func(o *ClientTransportOptions) {
+		o.Timeout = timeout
+	}
+}
