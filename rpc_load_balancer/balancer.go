1e50e8ff92a4b608fa3d1b32ed86707419dc67f1
kevinkwwei <kevinweikk@gmail.com>
2024-04-05 22:46:00 +0800

    load_balancer

--- rpc_load_balancer/balancer.go
+++ rpc_load_balancer/balancer.go
@@ -1,1 +1,48 @@
 package rpc_load_balancer
+
+type Node struct {
+	Key    string
+	Value  []byte
+	weight int
+}
+
+type Balancer interface {
+	Balance(string, []*Node) *Node
+}
+
+var balancerMap = make(map[string]Balancer, 0)
+
+const (
+	Random             = "random"
+	RoundRobin         = "roundRobin"
+	WeightedRoundRobin = "weightedRoundRobin"
+	ConsistentHash     = "consistentHash"
+
+	Custom = "custom"
+)
+
+func init() {
+	RegisterBalancer(Random, DefaultBalancer)
+	RegisterBalancer(RoundRobin, RRBalancer)
+	RegisterBalancer(WeightedRoundRobin, WRRBalancer)
+}
+
+func RegisterBalancer(name string, balancer Balancer) {
+	if balancerMap == nil {
+		balancerMap = make(map[string]Balancer)
+	}
+	balancerMap[name] = balancer
+}
+
+func GetBalancer(name string) Balancer {
+	if balancer, ok := balancerMap[name]; ok {
+		return balancer
+	}
+	return DefaultBalancer
+}
+
+var DefaultBalancer = newRandomBalancer()
+
+var RRBalancer = newRoundRobinBalancer()
+
+var WRRBalancer = newWeightedRoundRobinBalancer()
--- rpc_load_balancer/random_balancer.go
+++ rpc_load_balancer/random_balancer.go
@@ -1,1 +1,22 @@
 package rpc_load_balancer
+
+import (
+	"math/rand"
+	"time"
+)
+
+func newRandomBalancer() *randomBalancer {
+	return &randomBalancer{}
+}
+
+type randomBalancer struct {
+}
+
+func (r *randomBalancer) Balance(serviceName string, nodes []*Node) *Node {
+	if len(nodes) == 0 {
+		return nil
+	}
+	rand.Seed(time.Now().Unix())
+	num := rand.Intn(len(nodes))
+	return nodes[num]
+}
